b04bc2ea697f4a8969b1bb228f0599bf5740b45d
Zack Creach <zackcreach@gmail.com>
2018-10-14 22:00:38 -0400

    Progress with schema.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// let's go!
+require("dotenv").config({
+  path: "variables.env",
+});
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO User express middleware to handle cookies (JWT)
+// TODO User express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  (details) => {
+    console.log(
+      `Server is now running on port: http://localhost:${details.port}`,
+    );
+  },
+);
